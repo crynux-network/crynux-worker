1f3af77e26e48cc1f70780b36dd1a1023537f13e
iwehf <henry.lee@crynux.ai>
2024-12-02 17:33:17 +0800

    change config to adapt new pydantic-settings version

--- src/crynux_worker/config.py
+++ src/crynux_worker/config.py
@@ -1,67 +1,11 @@
 import os
-from typing import Any, Dict, List, Tuple, Type, Literal
+from typing import List, Tuple, Type, Literal
 
-import yaml
 from gpt_task import config as gpt_config
 from pydantic import BaseModel
-from pydantic.fields import FieldInfo
 from pydantic_settings import (BaseSettings, PydanticBaseSettingsSource,
-                               SettingsConfigDict)
+                               SettingsConfigDict, YamlConfigSettingsSource)
 from sd_task import config as sd_config
-
-
-class YamlSettingsConfigDict(SettingsConfigDict):
-    yaml_file: str | None
-
-
-class YamlConfigSettingsSource(PydanticBaseSettingsSource):
-    """
-    A simple settings source class that loads variables from a YAML file
-
-    Note: slightly adapted version of JsonConfigSettingsSource from docs.
-    """
-
-    _yaml_data: Dict[str, Any] | None = None
-
-    # def __init__(self, settings_cls: type[BaseSettings]):
-    #     super().__init__(settings_cls)
-
-    @property
-    def yaml_data(self) -> Dict[str, Any]:
-        if self._yaml_data is None:
-            yaml_file = self.config.get("yaml_file")
-            if yaml_file is not None and os.path.exists(yaml_file):
-                with open(yaml_file, mode="r", encoding="utf-8") as f:
-                    self._yaml_data = yaml.safe_load(f)
-            else:
-                self._yaml_data = {}
-        return self._yaml_data  # type: ignore
-
-    def get_field_value(
-        self, field: FieldInfo, field_name: str
-    ) -> Tuple[Any, str, bool]:
-        field_value = self.yaml_data.get(field_name)
-        return field_value, field_name, False
-
-    def prepare_field_value(
-        self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool
-    ) -> Any:
-        return value
-
-    def __call__(self) -> Dict[str, Any]:
-        d: Dict[str, Any] = {}
-
-        for field_name, field in self.settings_cls.model_fields.items():
-            field_value, field_key, value_is_complex = self.get_field_value(
-                field, field_name
-            )
-            field_value = self.prepare_field_value(
-                field_name, field, field_value, value_is_complex
-            )
-            if field_value is not None:
-                d[field_key] = field_value
-
-        return d
 
 
 class ModelsDirConfig(BaseModel):
@@ -115,11 +59,11 @@ class Config(BaseSettings):
     worker_url: str = "https://dy.relay.crynux.ai/v1/worker"
     output_dir: str = "results"
 
-    model_config = YamlSettingsConfigDict(
+    model_config = SettingsConfigDict(
         env_nested_delimiter="__",
         yaml_file=os.getenv("CRYNUX_WORKER_CONFIG", "config.yml"),
-        env_file=".env",
         env_prefix="cw_",
+        extra="ignore"
     )
 
     @classmethod
